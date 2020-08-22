b8dcceb2d2ac27939d8c2e8de78c546eef363fc8
winrid <winrid@gmail.com>
2020-08-22 13:38:48 -0700

    logs for state init and change

--- src/main/java/com/winricklabs/mouse/state/StateRunner.java
+++ src/main/java/com/winricklabs/mouse/state/StateRunner.java
@@ -1,12 +1,23 @@
 package com.winricklabs.mouse.state;
 
+import java.util.logging.Logger;
+
 public class StateRunner {
 
+    private static final Logger logger = Logger.getLogger(StateRunner.class.getName());
+
     public void run(MouseState initialState) {
+        logger.info("StateRunner BEGIN " + initialState.getClass().getName());
         MouseState state = initialState;
+        MouseState lastState = state;
         while (state != null) {
             state = state.run();
+            if (state != null && state != lastState) {
+                logger.info("StateRunner UPDATE " + lastState.getClass().getName() + " -> " + state.getClass().getName());
+                lastState = state;
+            }
         }
+        logger.info("StateRunner END");
     }
 
 }
