20ef8c2716061098254b72746f8fdce3c58a2dc9
yuxiushi <shiyuxiu2013@gmail.com>
2020-08-29 17:12:07 -0700

    I try github ui it is ok, and updateMotors v1

--- src/main/java/com/winricklabs/mouse/dto/UpdateMotors.java
+++ src/main/java/com/winricklabs/mouse/dto/UpdateMotors.java
@@ -1,6 +1,6 @@
 package com.winricklabs.mouse.dto;
 
 public class UpdateMotors {
-    public double leftMotor;
-    public double rightMotor;
+    public int leftMotor;
+    public int rightMotor;
 }
--- src/main/java/com/winricklabs/mouse/motor/MotorController.java
+++ src/main/java/com/winricklabs/mouse/motor/MotorController.java
@@ -1,9 +1,21 @@
 package com.winricklabs.mouse.motor;
 
+import com.pi4j.io.gpio.*;
+
 public class MotorController {
 
-    public static void updateMotors(double left, double right) {
-        // todo call library
+    public static void updateMotors(int left, int right) {
+        final GpioController gpio = GpioFactory.getInstance();
+
+        GpioPinPwmOutput meLeftFeetPin = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_01);
+        meLeftFeetPin.setPwmRange(100);
+
+        GpioPinPwmOutput meRightFeetPin = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_02);
+        meRightFeetPin.setPwmRange(100);
+
+        meLeftFeetPin.setPwm(left);
+        meRightFeetPin.setPwm(right);
+
     }
 
 }
