4854ce6f9a8dc50eaf517d202bf8d4bdf6183789
winrid <winrid@gmail.com>
2020-11-28 17:39:08 -0800

    fixing start path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -server -jar /home/pi/mouse/build/libs/all-in-one-jar-1.0-SNAPSHOT.jar >> /tmp/mouse.log 2>&1;
+java -server -jar /tmp/mouse/build/libs/all-in-one-jar-1.0-SNAPSHOT.jar >> /tmp/mouse.log 2>&1;
