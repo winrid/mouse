072fe725d0f79a709f9a6241b4ee58f02190bca3
winrid <winrid@gmail.com>
2021-01-09 15:00:12 -0800

    use bcm pins

--- src/main/java/com/winricklabs/mouse/motor/MotorController.java
+++ src/main/java/com/winricklabs/mouse/motor/MotorController.java
@@ -6,19 +6,19 @@ public class MotorController {
     // create gpio controller
     private static final GpioController gpio = GpioFactory.getInstance();
     // provision gpio pin #01 as an output pin and turn on
-    private static final GpioPinDigitalOutput leftOnPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, "MyLED", PinState.HIGH);
-    private static final GpioPinDigitalOutput leftOffPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "MyLED", PinState.LOW);
+    private static final GpioPinDigitalOutput leftOnPin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_17, "MyLED", PinState.HIGH);
+    private static final GpioPinDigitalOutput leftOffPin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_27, "MyLED", PinState.LOW);
 
-    private static final GpioPinDigitalOutput rightOnPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLED", PinState.HIGH);
-    private static final GpioPinDigitalOutput rightOffPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLED", PinState.LOW);
+    private static final GpioPinDigitalOutput rightOnPin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_06, "MyLED", PinState.HIGH);
+    private static final GpioPinDigitalOutput rightOffPin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_05, "MyLED", PinState.LOW);
     private static final GpioPinPwmOutput leftMotorSpeed;
     private static final GpioPinPwmOutput rightMotorSpeed;
 
     static {
-        leftMotorSpeed = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_13);
+        leftMotorSpeed = gpio.provisionSoftPwmOutputPin(RaspiBcmPin.GPIO_13);
         leftMotorSpeed.setPwmRange(1000);
 
-        rightMotorSpeed = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_22);
+        rightMotorSpeed = gpio.provisionSoftPwmOutputPin(RaspiBcmPin.GPIO_22);
         rightMotorSpeed.setPwmRange(1000);
 
         leftMotorSpeed.setPwm(500);
