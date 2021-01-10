3586b0b28c26d08e3328787112d8a17d3d204b45
winrid <winrid@gmail.com>
2021-01-09 17:19:29 -0800

    support reverse

--- src/main/java/com/winricklabs/mouse/motor/MotorController.java
+++ src/main/java/com/winricklabs/mouse/motor/MotorController.java
@@ -6,11 +6,11 @@ public class MotorController {
     // create gpio controller
     private static final GpioController gpio = GpioFactory.getInstance();
     // provision gpio pin #01 as an output pin and turn on
-    private static final GpioPinDigitalOutput leftOnPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
-    private static final GpioPinDigitalOutput leftOffPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.LOW);
+    private static final GpioPinDigitalOutput leftForwardPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "Left Forward", PinState.HIGH);
+    private static final GpioPinDigitalOutput leftReversePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Left Reverse", PinState.LOW);
 
-    private static final GpioPinDigitalOutput rightOnPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22, "MyLED", PinState.HIGH);
-    private static final GpioPinDigitalOutput rightOffPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_21, "MyLED", PinState.LOW);
+    private static final GpioPinDigitalOutput rightForwardPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22, "Right Forward", PinState.HIGH);
+    private static final GpioPinDigitalOutput rightReversePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_21, "Right Reverse", PinState.LOW);
     private static final GpioPinPwmOutput leftMotorSpeed;
     private static final GpioPinPwmOutput rightMotorSpeed;
 
@@ -18,21 +18,30 @@ public class MotorController {
         leftMotorSpeed = gpio.provisionPwmOutputPin(RaspiPin.GPIO_23);
         leftMotorSpeed.setPwmRange(1000);
 
-
         rightMotorSpeed = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_03);
         rightMotorSpeed.setPwmRange(1000);
 
         leftMotorSpeed.setPwm(100);
         rightMotorSpeed.setPwm(100);
-        System.err.println("Yuxiu: static block runs");
-
+        System.out.println("Static block runs");
     }
 
     public static void updateMotors(int leftInputSpeed, int rightInputSpeed) {
-        leftMotorSpeed.setPwm(leftInputSpeed);
-        rightMotorSpeed.setPwm(rightInputSpeed);
-        System.err.println("Yuxiu: updated motor with speed :  "+leftInputSpeed + ", "+rightInputSpeed);
+        System.out.println(String.format("Updated motor speed left=[%s] right=[%s]", leftInputSpeed, rightInputSpeed));
+        updateMotorSpeed(leftForwardPin, leftReversePin, leftMotorSpeed, leftInputSpeed);
+        updateMotorSpeed(rightForwardPin, rightReversePin, rightMotorSpeed, rightInputSpeed);
+    }
 
+    private static void updateMotorSpeed(GpioPinDigitalOutput forwardPin, GpioPinDigitalOutput reversePin, GpioPinPwmOutput speedPin, int speed) {
+        // If positive, consider that as going forward
+        if (speed >= 0) {
+            forwardPin.high();
+            reversePin.low();
+        } else {
+            forwardPin.low();
+            reversePin.high();
+        }
+        speedPin.setPwm(speed);
     }
 
 }
