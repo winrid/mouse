f1bd1859af8a7b246c6f5ee3712731a60ba1952a
yuxiushi <shiyuxiu2013@gmail.com>
2021-01-09 17:01:26 -0800

    update the pin number with the wiringPI pin

--- src/main/java/com/winricklabs/mouse/motor/MotorController.java
+++ src/main/java/com/winricklabs/mouse/motor/MotorController.java
@@ -6,33 +6,31 @@ public class MotorController {
     // create gpio controller
     private static final GpioController gpio = GpioFactory.getInstance();
     // provision gpio pin #01 as an output pin and turn on
-    private static final GpioPinDigitalOutput leftOnPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "left motor forward", PinState.HIGH);
-    private static final GpioPinDigitalOutput leftOffPin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_27, "left motor backward", PinState.LOW);
+    private static final GpioPinDigitalOutput leftOnPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
+    private static final GpioPinDigitalOutput leftOffPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.LOW);
 
-    private static final GpioPinDigitalOutput rightOnPin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_06, "right motor forward", PinState.HIGH);
-    private static final GpioPinDigitalOutput rightOffPin = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_05, "right motor backward", PinState.LOW);
+    private static final GpioPinDigitalOutput rightOnPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_22, "MyLED", PinState.HIGH);
+    private static final GpioPinDigitalOutput rightOffPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_21, "MyLED", PinState.LOW);
     private static final GpioPinPwmOutput leftMotorSpeed;
-//    private static final GpioPinPwmOutput rightMotorSpeed;
+    private static final GpioPinPwmOutput rightMotorSpeed;
 
     static {
-        leftOnPin.setShutdownOptions(true, PinState.LOW);
-        leftOnPin.high();
-        leftOffPin.low();
-        leftMotorSpeed = gpio.provisionPwmOutputPin(RaspiBcmPin.GPIO_13);
+        leftMotorSpeed = gpio.provisionPwmOutputPin(RaspiPin.GPIO_23);
         leftMotorSpeed.setPwmRange(1000);
 
-//        rightMotorSpeed = gpio.provisionPwmOutputPin(RaspiBcmPin.GPIO_22);
-//        rightMotorSpeed.setPwmRange(100);
 
-        leftMotorSpeed.setPwm(500);
-//        rightMotorSpeed.setPwm(100);
+        rightMotorSpeed = gpio.provisionPwmOutputPin(RaspiPin.GPIO_03);
+        rightMotorSpeed.setPwmRange(1000);
+
+        leftMotorSpeed.setPwm(100);
+        rightMotorSpeed.setPwm(100);
         System.err.println("Yuxiu: static block runs");
 
     }
 
     public static void updateMotors(int leftInputSpeed, int rightInputSpeed) {
         leftMotorSpeed.setPwm(leftInputSpeed);
-//        rightMotorSpeed.setPwm(rightInputSpeed);
+        rightMotorSpeed.setPwm(rightInputSpeed);
         System.err.println("Yuxiu: updated motor with speed :  "+leftInputSpeed + ", "+rightInputSpeed);
 
     }
