f8d61d049dbe2ab762d1c72e669aa74a890183fb
winrid <winrid@gmail.com>
2021-01-18 15:06:15 -0800

    indicate when service starts

--- src/main/java/com/winricklabs/mouse/Main.java
+++ src/main/java/com/winricklabs/mouse/Main.java
@@ -1,18 +1,36 @@
 package com.winricklabs.mouse;
 
+import com.pi4j.io.gpio.*;
 import com.winricklabs.mouse.state.StateRunner;
 import com.winricklabs.mouse.state.Wakeup;
 import com.winricklabs.mouse.websocket.MouseSocketServer;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
     private static final int PORT = 8035;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MouseSocketServer server = new MouseSocketServer( PORT);
         server.start();
         System.out.println("Server Started on " + PORT);
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // Flash our LED on startup.
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "MyLED", PinState.HIGH);
+        pin.setShutdownOptions(true, PinState.LOW);
+        pin.high();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                pin.low();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 1500);
 
 //        new StateRunner().run(new Wakeup());
     }
