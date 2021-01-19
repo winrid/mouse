84d38b9e38fff73e25b28d5b1928b11c847ec4d1
winrid <winrid@gmail.com>
2021-01-18 16:39:27 -0800

    fixing install dir

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo java -server -jar /tmp/mouse/build/libs/all-in-one-jar-1.0-SNAPSHOT.jar >> /tmp/mouse.log 2>&1;
+sudo java -server -jar /home/pi/mouse/build/libs/all-in-one-jar-1.0-SNAPSHOT.jar >> /tmp/mouse.log 2>&1;
